d5e3d3fe5398c9d3d23bc23aff766daf45d7de71
Kinn <neraninii@gmail.com>
2026-03-09 11:08:40 -0400

    Got the new input system working, moved and added SFX

---
+++ Assets/Scripts/Controller Inputs.cs
@@ -0,0 +1,44 @@
+
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInputs : MonoBehaviour
+{
+    public float speed = 5; 
+    public Vector2 movement; 
+    public AudioSource SFX;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += (Vector3)movement * speed * Time.deltaTime; 
+        transform.position = movement;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //this is the same as Mouse.current.position.ReadValue()
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>()); 
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>(); 
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack! " + context.phase);
+        if(context.performed == true)
+        {
+            SFX.Play();
+        }
+    }
+}
