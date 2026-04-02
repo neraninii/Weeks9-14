5669fea929ad2234f1e501d04b4ce0b1e5ac4282
Kinn <neraninii@gmail.com>
2026-04-02 03:57:28 -0400

    added heart function

--- Assets/Systems Task/Scripts/heart.cs
+++ Assets/Systems Task/Scripts/heart.cs
@@ -1,16 +1,59 @@
+using System.Collections;
 using UnityEngine;
 
 public class heart : MonoBehaviour
 {
+
+    public Transform hearts; 
+
+    public SpriteRenderer NPC;
+
+    Coroutine heartCoroutine; 
+    Coroutine givingCoroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hearts.localScale = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GiveHeart()
+    {
+    
+         if (NPC.bounds.Contains(transform.position) == true)
+        {
+            StopCoroutine(givingCoroutine);
+        }
+
+        givingCoroutine = StartCoroutine(HeartSpawn());
+    }
+
+    IEnumerator HeartSpawn()
+    {
+        Debug.Log("heart start");
+        float t = 0; 
+        hearts.localScale = Vector2.zero;
+        
+        while(t < 1)
+        {
+            t += Time.deltaTime;
+            hearts.localScale = Vector2.one * t;
+            yield return null;
+
+        }
+        Debug.Log("heart given");
+
+        
+    }
+
+    IEnumerator spawning()
+    {
+        yield return heartCoroutine = StartCoroutine(HeartSpawn());
     }
 }
